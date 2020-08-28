9755c5de2b93d2cc366bb2b121da930934c674a7
Cronvic <68031651+Cronvic@users.noreply.github.com>
2020-08-27 21:01:05 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,712 @@
+// Deck
+
+let deck = [
+    {
+       card: "Ace of Cups",
+       id: 0,
+       desc: "Flaws in realationships.",
+       url: "'imgs/ace-of-cups.png'"
+    },
+
+    {
+        card: "Two of Cups",
+        id: 1,
+        desc: "Coming together in harmoy.",
+        url: "'imgs/two-of-cups.png'"
+    },
+
+    {
+        card: "Three of Cups",
+        id: 2,
+        desc: "A sense of community.",
+        url: "'imgs/three-of-cups.png'"
+    },
+
+    {
+        card: "Four of Cups",
+        id: 3,
+        desc: "Disillusionment and a sense of remose",
+        url: "'imgs/four-of-cups.png'"
+    },
+
+    {
+        card: "Five of Cups",
+        id: 4,
+        desc: "Disapointment over past events.",
+        url: "'imgs/five-of-cups.png'"
+    },
+
+    {
+        card: "Six of Cups",
+        id: 5,
+        desc: "Reminiscing over past events, a sense of nostalgia.",
+        url: "'imgs/six-of-cups.png'"
+    },
+
+    {
+        card: "Seven of Cups",
+        id: 6,
+        desc: "Having many choices to choose from.",
+        url: "'imgs/seven-of-cups.png'"
+    },
+
+    {
+        card: "Eight of Cups",
+        id: 7,
+        desc: "Turning away from the present.",
+        url: "'imgs/eight-of-cups.png'"
+    },
+
+    {
+        card: "Nine of Cups",
+        id: 8,
+        desc: "A feeling of contentment.",
+        url: "'imgs/nine-of-cups.png'"
+    },
+
+    {
+        card: "Ten of Cups",
+        id: 9,
+        desc: "One of the most positive cards, complete contentment with the present.",
+        url: "'imgs/ten-of-cups.png'"
+    },
+
+    {
+        card: "Page of Cups",
+        id: 10,
+        desc: "Carefree life of a child.",
+        url: "'imgs/page-of-cups.png'"  
+    },
+
+    {
+        card: "Knight of Cups",
+        id: 11,
+        desc: "Change and new excitments ariseing.",
+        url: "'imgs/knight-of-cups.png'"
+    },
+
+    {
+        card: "Queen of Cups",
+        id: 12,
+        desc: "A loyal friend.",
+        url: "'imgs/queen-of-cups.png'"
+    },
+
+    {
+        card: "King of Cups",
+        id: 13,
+        desc: "The creative spirit.",
+        url: "'imgs/king-of-cups.png'"
+    },
+
+    {
+        card: "Ace of Wands",
+        id: 14,
+        desc: "Taking action and new horizons.",
+        url: "'imgs/ace-of-wands.png'"
+    },
+
+    {
+        card: "Two of Wands",
+        id: 15,
+        desc: "Having two paths to take before you.",
+        url: "'imgs/two-of-wands.png'"
+    },
+
+    {
+        card: "Three of Wands",
+        id: 16,
+        desc: "Looking forward to the future with optimism.",
+        url: "'imgs/three-of-wands.png'"
+    },
+
+    {
+        card: "Four of Wands",
+        id: 17,
+        desc: "Hard work will be met with equal results.",
+        url: "'imgs/four-of-wands.png'"
+    },
+
+    {
+        card: "Five of Wands",
+        id: 18,
+        desc: "Conflict and clashing of egos.",
+        url: "'imgs/five-of-wands.png'"
+    },
+
+    {
+        card: "Six of Wands",
+        id: 19,
+        desc: "Having the advantage.",
+        url: "'imgs/six-of-wands.png'"
+    },
+
+    {
+        card: "Seven of Wands",
+        id: 20,
+        desc: "Fighting for your territory.",
+        url: "'imgs/seven-of-wands.png'"
+    },
+      
+    {
+        card: "Eight of Wands",
+        id: 21,
+        desc: "Swift action.",
+        url: "'imgs/eight-of-wands.png'"
+    },
+
+    {
+        card: "Nine of Wands",
+        id: 22,
+        desc: "Keep going, your almost there.",
+        url: "'imgs/nine-of-wands.png'"
+    },
+
+    {
+        card: "Ten of Wands",
+        id: 23,
+        desc: "Burdening situtions.",
+        url: "'imgs/ten-of-wands.png'"
+    },
+
+    {
+        card: "Page of Wands",
+        id: 24,
+        desc: "Inspiring news is coming.",
+        url: "'imgs/page-of-wands.png'"
+    },
+
+    {
+        card: "Knight of Wands",
+        id: 25,
+        desc: "New ideas and progress.",
+        url: "'imgs/knight-of-wands.png'"
+    },
+
+    {
+        card: "Queen of Wands",
+        id: 26,
+        desc: "Optimistic and full of energy.",
+        url: "'imgs/queen-of-wands.png'"
+    },
+
+    {
+        card: "King of Wands",
+        id: 27,
+        desc: "Taking control of your life.",
+        url: "'imgs/king-of-wands.png'"
+    },
+
+    {
+        card: "Ace of Swords",
+        id: 28,
+        desc: "Using great force.",
+        url: "'imgs/ace-of-swords.png'"
+    },
+    
+    {
+        card: "Two of Swords",
+        id: 29,
+        desc: "Facing your fears.",
+        url: "'imgs/two-of-swords.png'"
+    },
+
+    {
+        card: "Three of Swords",
+        id: 30,
+        desc: "A sorrowful experience.",
+        url: "'imgs/three-of-swords.png'"
+    },
+
+    {
+        card: "Four of Swords",
+        id: 31,
+        desc: "Great purpose and devotion.",
+        url: "'imgs/four-of-swords.png'"
+    },
+
+    {
+        card: "Five of Swords",
+        id: 32,
+        desc: "Defeat.",
+        url: "'imgs/five-of-swords.png'"
+    },
+
+    {
+        card: "Six of Swords",
+        id: 33,
+        desc: "Gradual change, a solution to current problems.",
+        url: "'imgs/six-of-swords.png'"
+    },
+
+    {
+        card: "Seven of Swords",
+        id: 34,
+        desc: "Using wits rather then force.",
+        url: "'imgs/seven-of-swords.png'"
+    },
+
+    {
+        card: "Eight of Swords",
+        id: 35,
+        desc: "Trapped, unable to move for fear of being hurt.",
+        url: "'imgs/eight-of-swords.png'"
+    },
+
+    {
+        card: "Nine of Swords",
+        id: 36,
+        desc: "Your greatest fears are unlikely to hurt you.",
+        url: "'imgs/nine-of-swords.png'"
+    },
+
+    {
+        card: "Ten of Swords",
+        id: 37,
+        desc: "Things are bad but they can improve.",
+        url: "'imgs/ten-of-swords.png'"
+    },
+
+    {
+        card: "Page of Swords",
+        id: 38,
+        desc: "Be patient.",
+        url: "'imgs/page-of-swords.png'"
+    },
+
+    {
+        card: "Knight of Swords",
+        id: 39,
+        desc: "Being rash.",
+        url: "'imgs/knight-of-swords.png'"
+    },
+
+    {
+        card: "Queen of Swords",
+        id: 40,
+        desc: "You will receive criticism",
+        url: "'imgs/queen-of-swords.png'"
+    },
+
+    {
+        card: "King of Swords",
+        id: 41,
+        desc: "Mental clarity.",
+        url: "'imgs/king-of-swords.png'"
+    },
+
+    {
+        card: "Ace of Pentacles",
+        id: 42,
+        desc: "Manifesting your goals.",
+        url: "'imgs/ace-of-pentacles.png'"
+    },
+
+    {
+        card: "Two of Pentacles",
+        id: 43,
+        desc: "To maintain balance.",
+        url: "'imgs/two-of-pentacles.png'"
+    },
+
+    {
+        card: "Three of Pentacles",
+        id: 44,
+        desc: "Mastery of a skill.",
+        url: "'imgs/three-of-pentacles.png'"
+    },
+
+    {
+        card: "Four of Pentacles",
+        id: 45,
+        desc: "Hoarding wealth.",
+        url: "'imgs/four-of-pentacles.png'"
+    },
+
+    {
+        card: "Five of Pentacles",
+        id: 46,
+        desc: "A hard sitution that will remain.",
+        url: "'imgs/five-of-pentacles.png'"
+    },
+
+    {
+        card: "Six of Pentacles",
+        id: 47,
+        desc: "Charity.",
+        url: "'imgs/six-of-pentacles.png'"
+    },
+
+    {
+        card: "Seven of Pentacles",
+        id: 48,
+        desc: "A card of movement.",
+        url: "'imgs/seven-of-pentacles.png'"
+    },
+
+    {
+        card: "Eight of Pentacles",
+        id: 49,
+        desc: "Development of skills.",
+        url: "'imgs/eight-of-pentacles.png'"
+    },
+
+    {
+        card: "Nine of Pentacles",
+        id: 50,
+        desc: "Steady security.",
+        url: "'imgs/nine-of-pentacles.png'"
+    },
+
+    {
+        card: "Ten of Pentacles",
+        id: 51,
+        desc: "Long term success.",
+        url: "'imgs/ten-of-pentacles.png'"
+    },
+
+    {
+        card: "Page of Pentacles",
+        id: 52,
+        desc: "Change in your work life.",
+        url: "'imgs/page-of-pentacles.png'"
+    },
+
+    {
+        card: "Knight of Pentacles",
+        id: 53,
+        desc: "Strong routine.",
+        url: "'imgs/knight-of-pentacles.png'"
+    },
+
+    {
+        card: "Queen of Pentacles",
+        id: 54,
+        desc: "Taking responsibility.",
+        url: "'imgs/queen-of-pentacles.png'"
+    },
+
+    {
+        card: "King of Pentacles",
+        id: 55,
+        desc: "Hard work will pay off.",
+        url: "'imgs/king-of-pentacles.png'"
+    },
+
+    {
+        card: "The Fool",
+        id: 56,
+        desc: "New beginnings.",
+        url: "'imgs/the-fool.png'"
+    },
+
+    {
+        card: "The Magician",
+        id: 57,
+        desc: "Your talents and capabilities.",
+        url: "'imgs/the-magician.png'"
+    },
+
+    {
+        card: "The High Priestess",
+        id: 58,
+        desc: "Trust your instincts.",
+        url: "'imgs/the-high-priestess.png'"
+    },
+
+    {
+        card: "The Empress",
+        id: 59,
+        desc: "Creation.",
+        url: "'imgs/the-empress.png'"
+    },
+
+    {
+        card: "The Emperor",
+        id: 60,
+        desc: "Conviction.",
+        url: "'imgs/the-emperor.png'"
+    },
+
+    {
+        card: "The Hierophant",
+        id: 61,
+        desc: "Inspiration.",
+        url: "'imgs/the-hierophant.png'"
+    },
+
+    {
+        card: "The Lovers",
+        id: 62,
+        desc: "Trials overcome.",
+        url: "'imgs/the-lovers.png'"
+    },
+
+    {
+        card: "The Chariot",
+        id: 63,
+        desc: "Control.",
+        url: "'imgs/the-chariot.png'"
+    },
+
+    {
+        card: "Justice",
+        id: 64,
+        desc: "Truth.",
+        url: "'imgs/justice.png'"
+    },
+
+    {
+        card: "The Hermit",
+        id: 65,
+        desc: "Introspection.",
+        url: "'imgs/the-hermit.png'"
+    },
+
+    {
+        card: "Wheel of Fortune",
+        id: 66,
+        desc: "A change in ones life.",
+        url: "'imgs/wheel-of-fortune.png'"
+    },
+
+    {
+        card: "Strength",
+        id: 67,
+        desc: "Courage.",
+        url: "'imgs/strength.png'"
+    },
+
+    {
+        card: "The Hanged Man",
+        id: 68,
+        desc: "Letting go.",
+        url: "'imgs/the-hanged-man.png'"
+    },
+
+    {
+        card: "Death",
+        id: 69,
+        desc: "Transition.",
+        url: "'imgs/death.png'"
+    },
+
+    {
+        card: "Temperance",
+        id: 70,
+        desc: "Patience.",
+        url: "'imgs/temperance.png'"
+    },
+
+    {
+        card: "The Devil",
+        id: 71,
+        desc: "Sudduced.",
+        url: "'imgs/the-devil.png'"
+    },
+
+    {
+        card: "The Tower",
+        id: 72,
+        desc: "Ruin.",
+        url: "'imgs/the-tower.png'"
+    },
+
+    {
+        card: "The Star",
+        id: 73,
+        desc: "Hope.",
+        url: "'imgs/the-star.png'"
+    },
+
+    {
+        card: "The Moon",
+        id: 74,
+        desc: "Illusion.",
+        url: "'imgs/the-moon.png'"
+    },
+
+    {
+        card: "The Sun",
+        id: 75,
+        desc: "Positivity.",
+        url: "'imgs/the-sun.png'"
+    },
+
+    {
+        card: "Judgement",
+        id: 76,
+        desc: "Absolving.",
+        url: "'imgs/judgement.png'"
+    },
+
+    {
+        card: "The World",
+        id: 77,
+        desc: "Completion.",
+        url: "'imgs/the-world.png'"
+    }
+];
+
+// Variables
+pickedCards = [];
+pickedCardsNum = [];
+
+
+// Selectors
+
+const cards = document.querySelector(".cards");
+const card = document.querySelectorAll(".card");
+
+const getReading = document.querySelector(".get-reading");
+
+const card0 = document.querySelector('.card' + deck[0].id);
+const card1 = document.querySelector('.card' + deck[1].id);
+const card2 = document.querySelector('.card' + deck[2].id);
+const card3 = document.querySelector('.card' + deck[3].id);
+const card4 = document.querySelector('.card' + deck[4].id);
+const card5 = document.querySelector('.card' + deck[5].id);
+const card6 = document.querySelector('.card' + deck[6].id);
+const card7 = document.querySelector('.card' + deck[7].id);
+const card8 = document.querySelector('.card' + deck[8].id);
+const card9 = document.querySelector('.card' + deck[9].id);
+const card10 = document.querySelector('.card' + deck[10].id);
+const card11 = document.querySelector('.card' + deck[11].id);
+const card12 = document.querySelector('.card' + deck[12].id);
+const card13 = document.querySelector('.card' + deck[13].id);
+const card14 = document.querySelector('.card' + deck[14].id);
+const card15 = document.querySelector('.card' + deck[15].id);
+const card16 = document.querySelector('.card' + deck[16].id);
+const card17 = document.querySelector('.card' + deck[17].id);
+const card18 = document.querySelector('.card' + deck[18].id);
+const card19 = document.querySelector('.card' + deck[19].id);
+const card20 = document.querySelector('.card' + deck[20].id);
+const card21 = document.querySelector('.card' + deck[21].id);
+const card22 = document.querySelector('.card' + deck[22].id);
+const card23 = document.querySelector('.card' + deck[23].id);
+const card24 = document.querySelector('.card' + deck[24].id);
+const card25 = document.querySelector('.card' + deck[25].id);
+const card26 = document.querySelector('.card' + deck[26].id);
+const card27 = document.querySelector('.card' + deck[27].id);
+const card28 = document.querySelector('.card' + deck[28].id);
+const card29 = document.querySelector('.card' + deck[29].id);
+const card30 = document.querySelector('.card' + deck[30].id);
+const card31 = document.querySelector('.card' + deck[31].id);
+const card32 = document.querySelector('.card' + deck[32].id);
+const card33 = document.querySelector('.card' + deck[33].id);
+const card34 = document.querySelector('.card' + deck[34].id);
+const card35 = document.querySelector('.card' + deck[35].id);
+const card36 = document.querySelector('.card' + deck[36].id);
+const card37 = document.querySelector('.card' + deck[37].id);
+const card38 = document.querySelector('.card' + deck[38].id);
+const card39 = document.querySelector('.card' + deck[39].id);
+const card40 = document.querySelector('.card' + deck[40].id);
+const card41 = document.querySelector('.card' + deck[41].id);
+const card42 = document.querySelector('.card' + deck[42].id);
+const card43 = document.querySelector('.card' + deck[43].id);
+const card44 = document.querySelector('.card' + deck[44].id);
+const card45 = document.querySelector('.card' + deck[45].id);
+const card46 = document.querySelector('.card' + deck[46].id);
+const card47 = document.querySelector('.card' + deck[47].id);
+const card48 = document.querySelector('.card' + deck[48].id);
+const card49 = document.querySelector('.card' + deck[49].id);
+const card50 = document.querySelector('.card' + deck[50].id);
+const card51 = document.querySelector('.card' + deck[51].id);
+const card52 = document.querySelector('.card' + deck[52].id);
+const card53 = document.querySelector('.card' + deck[53].id);
+const card54 = document.querySelector('.card' + deck[54].id);
+const card55 = document.querySelector('.card' + deck[55].id);
+const card56 = document.querySelector('.card' + deck[56].id);
+const card57 = document.querySelector('.card' + deck[57].id);
+const card58 = document.querySelector('.card' + deck[58].id);
+const card59 = document.querySelector('.card' + deck[59].id);
+const card60 = document.querySelector('.card' + deck[60].id);
+const card61 = document.querySelector('.card' + deck[61].id);
+const card62 = document.querySelector('.card' + deck[62].id);
+const card63 = document.querySelector('.card' + deck[63].id);
+const card64 = document.querySelector('.card' + deck[64].id);
+const card65 = document.querySelector('.card' + deck[65].id);
+const card66 = document.querySelector('.card' + deck[66].id);
+const card67 = document.querySelector('.card' + deck[67].id);
+const card68 = document.querySelector('.card' + deck[68].id);
+const card69 = document.querySelector('.card' + deck[69].id);
+const card70 = document.querySelector('.card' + deck[70].id);
+const card71 = document.querySelector('.card' + deck[71].id);
+const card72 = document.querySelector('.card' + deck[72].id);
+const card73 = document.querySelector('.card' + deck[73].id);
+const card74 = document.querySelector('.card' + deck[74].id);
+const card75 = document.querySelector('.card' + deck[75].id);
+const card76 = document.querySelector('.card' + deck[76].id);
+const card77 = document.querySelector('.card' + deck[77].id);
+
+
+
+const selectedCards = [card0, card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12, card13, card14, card15, card16, card17, card18, card19, card20, card21, card22, card23, card24, card25, card26, card27, card28, card29, card30, card31, card32, card33, card34, card35, card36, card37, card38, card39, card40, card41, card42, card43, card44, card45, card46, card47, card48, card49, card50, card51, card52, card53, card54, card55, card56, card57, card58, card59, card60, card61, card62, card63, card64, card65, card66, card67, card68, card69, card70, card71, card72, card73, card74, card75, card76, card77];
+
+// Functions
+
+const shuffleDeck = (deck) => {
+    let count = deck.length;
+    while(count) {
+        deck.push(deck.splice(Math.floor(Math.random() * count), 1)[0]);
+        count -= 1;
+    }
+}
+
+const fanCards = () => {
+    for (i = 0; i < deck.length; i++){
+        var r = 2 * i;
+        selectedCards[i].style.transform = 'rotate(' + r + 'deg)';      
+    }
+}
+
+
+const displayReading = () => {
+    for (i = 0; i < pickedCardsNum.length; i++) {
+        cards.style.display = 'none';
+        getReading.style.display = 'none';
+        
+        var reading = document.createElement('div');
+        reading.classList.add('reading');
+        reading.innerHTML ="<img src=" + deck[pickedCardsNum[i]].url + "width=\"114px\" height=\"180px\">";;
+        document.body.appendChild(reading);
+
+        var description = document.createElement('div');
+        description.classList.add('description');
+        description.innerText = deck[pickedCardsNum[i]].desc;
+        document.body.appendChild(description);
+
+
+        console.log(deck[pickedCardsNum[i]].card);
+
+    }
+}
+
+
+
+// Event listeners
+
+selectedCards.forEach(function (element){
+    element.addEventListener("click", function(){
+        picked = parseInt(element.className.match(/\d+/));
+        element.classList.add("pickedCard");
+        element.style.transform = 'translate(-300px, -130px) rotate(105deg)';
+        pickedCards.push(element);
+        pickedCardsNum.push(picked);
+        console.log(deck[element.className.match(/\d+/)]);
+    });
+  });
+
+  getReading.addEventListener('click', displayReading);
+
+
+// Scripts
+
+
+
+shuffleDeck(deck);
+fanCards();
+
+
+
+
